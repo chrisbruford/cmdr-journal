47dfbd6f1c83425724c0d7dd9fd623c98a55b5b5
Chris Bruford <chrisbruford@gmail.com>
2018-04-29 17:16:29 +0100

    refactor exports and docs

--- src/index.ts
+++ src/index.ts
@@ -4,5 +4,9 @@
  * test2 comment
  */
 export * from './models';
-export { Faction } from './models/faction.model';
 export * from './enums';
+
+import * as models from './models';
+import * as enums from './enums';
+
+export default Object.assign({},models,enums);

d95ed5a7a8695b4448279535c364c995eb07075f
Chris Bruford <chrisbruford@gmail.com>
2018-05-07 00:22:08 +0100

    fix: change redeem-vouchers Factions prop to be an array of objects

---
+++ src/models/redeem-voucher.ts
@@ -0,0 +1,32 @@
+import { JournalEvent } from "./journal-event.model";
+
+/**
+ * When Written: when claiming payment for combat bounties and bonds
+ * 
+ * ## Example
+ * ```javascript
+ * { 
+ *     "timestamp":"2016-06-10T14:32:03Z", 
+ *     "event":"RedeemVoucher", 
+ *     "Type":"bounty", 
+ *     Factions: [ 
+ *         { 
+ *             "Faction":"Ed's 38", 
+ *             "Amount":1000 
+ *         }, 
+ *         { 
+ *             "Faction":"Zac's Lads", 
+ *             "Amount": 2000 
+ *         } 
+ *     ] 
+ * }
+ * ```
+ */
+
+export class RedeemVoucher extends JournalEvent {
+    Type: string;
+    Amount: number;
+    Faction?: string; //not for bounties
+    BrokerPercentage?: number;
+    Factions?: {Faction:string; Amount: number}[] //for Bounties
+}

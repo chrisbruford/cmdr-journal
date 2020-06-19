445a71a162ea3aef43eda49a4216f93b4f58e6f2
Jorerlol <ljorer@yahoo.com.au>
2020-06-19 21:15:13 +1000

    Create carrier-buy.ts

---
+++ src/models/carrier-buy.ts
@@ -0,0 +1,18 @@
+import { JournalEvent } from "./journal-event.model";
+
+/**
+ * When written: Player buys a fleet carrier
+ * { "timestamp":"2020-03-11T15:31:46Z", "event":"CarrierBuy", "CarrierID":3700029440, "BoughtAtMarket":3221301504,
+ * "Location":"Kakmbutan", "SystemAddress":3549513615723, "Price":4875000000, "Variant":"CarrierDockB",
+ * "Callsign":"P07-V3L" } 
+
+
+export class CarrierBuy extends JournalEvent {
+    BoughtAtMarket: number;
+    CarrierID: number;
+    Location: string;
+    SystemAddress: number;
+    Price: number;
+    Variant: string;
+    Callsign: string;
+}

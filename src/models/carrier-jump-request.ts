40aa820726a7a28df9935f7300a21b10c0e4208a
Jorerlol <ljorer@yahoo.com.au>
2020-06-19 21:51:08 +1000

    Create carrier-jump-request.ts

---
+++ src/models/carrier-jump-request.ts
@@ -0,0 +1,20 @@
+import { JournalEvent } from "./journal-event.model";
+
+/** 
+ *Written when a carrier makes a jump request. Not the jump.
+ *{ "timestamp":"2020-04-20T09:30:58Z",
+ *"event":"CarrierJumpRequest",
+ *"CarrierID":3700005632,
+ *SystemName":"Paesui Xena",
+ *"Body":"Paesui Xena A",
+ *"SystemAddress":7269634680241,
+ *"BodyID":1 }
+ */
+
+export class Location extends JournalEvent {
+	CarrierID: number;
+	SystemName: string;
+	Body: string;
+	SystemAddress: number;
+	BodyID: number;
+}
